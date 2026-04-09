49a68d8dbdcc2aed8e7b905966192d3d0eb5dda9
mateuspigg-ui <274306280+mateuspigg-ui@users.noreply.github.com>
2026-04-09 15:01:51 -0300

    fix: lock interface language to Portuguese

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+document.documentElement.lang = "pt-BR";
+document.documentElement.setAttribute("translate", "no");
+document.documentElement.classList.add("notranslate");
+
 createRoot(document.getElementById("root")!).render(<App />);
